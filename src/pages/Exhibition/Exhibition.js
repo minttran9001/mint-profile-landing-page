8ce2a574f45e7fdea897f9a770e8c3565585ceba
Mint Project <minttran.9002@gmail.com>
2021-03-30 19:13:57 +0700

    Add download cv

--- src/pages/Exhibition/Exhibition.js
+++ src/pages/Exhibition/Exhibition.js
@@ -5,8 +5,8 @@ import paint1 from "../../images/100ppi/paint1.png";
 import paint2 from "../../images/100ppi/paint2.png";
 import paint3 from "../../images/100ppi/paint3.png";
 import paint4 from "../../images/100ppi/paint4.png";
-import Button from "../../components/Button/Button"
-import { NavLink } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import { Link, NavLink } from "react-router-dom";
 import PageTransition from "../../layout/PageTransition/PageTransition";
 
 export default class Exhibition extends Component {
@@ -14,23 +14,36 @@ export default class Exhibition extends Component {
     return (
       <PageTransition>
         <Header>
-        <div className="exhibition-section landing-page section">
-          <div className="qoute-container container">
-            <div className="exhibition-qoute">
+          <div className="exhibition-section landing-page section">
+            <div className="qoute-container container">
+              <div className="exhibition-qoute">
                 <div className="qoute">
-                    <p>Many of life’s failures are people who did not realize how close they were to success when they gave up.</p>
+                  <p>
+                    Many of life’s failures are people who did not realize how
+                    close they were to success when they gave up.
+                  </p>
                 </div>
-                <NavLink className='qoute-link' to='/about'><Button>Explore</Button></NavLink>
-            </div>
-            <div className="paint-background">
-              <img className="paint-1" src={paint1} alt="paint" />
-              <img className="paint-2" src={paint2} alt="paint" />
-              <img className="paint-3" src={paint3} alt="paint" />
-              <img className="paint-4" src={paint4} alt="paint" />
+                <NavLink className="qoute-link" to="/about">
+                  <Button>Explore</Button>
+                  
+                </NavLink>
+                <Link
+                  className="qoute-link"
+                  to="/cv/TrầnThanhMinh-Intern-Frontend.pdf"
+                  target="_blank" download
+                >
+                  <button className="download-btn">Download my CV</button>
+                </Link>
+              </div>
+              <div className="paint-background">
+                <img className="paint-1" src={paint1} alt="paint" />
+                <img className="paint-2" src={paint2} alt="paint" />
+                <img className="paint-3" src={paint3} alt="paint" />
+                <img className="paint-4" src={paint4} alt="paint" />
+              </div>
             </div>
           </div>
-        </div>
-      </Header>
+        </Header>
       </PageTransition>
     );
   }
